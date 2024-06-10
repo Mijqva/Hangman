e8df5b638fae7c7fa365eecac907b5550ac88a50
Mikhail Pashkov <mikhail.pashkov@raiffeisen.ru>
2024-06-10 15:59:21 +0300

    111

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException{
 
         System.out.println("Это игра Виселица. Чтобы начать игру, введите Start");
+        // paintMan(7);
 
         String inpVal = new String(readConsole());
         //System.out.println(inpVal);
@@ -18,40 +19,53 @@ public class Main {
             System.out.println("Неверная команда, выполнение прекращено");
             System.exit(0);
         }
-
-
     }
 
     private static void iterateOverWord(String Word) throws IOException{
         char[] arrWord = Word.toCharArray();
         char[] arrHiddenWord = ReplaceChars(Word).toCharArray();
+        int numOfErrors = 0;
+        int numOfSuccess = 0;
 
         System.out.println(arrHiddenWord);
 
-        for (int i=0;i<7;i++){
+        for (;;){
             System.out.println("Введите букву");
             char currChar = readConsole().charAt(0);//не забыть отвалидировать инпут
             boolean isHit = false;
+
             for (int j=0;j<arrWord.length;j++){
                 if (currChar == arrWord[j]){
                     arrHiddenWord[j]=currChar;
+                    ++numOfSuccess;
                     isHit = true;
                 }
-
             }
+
             if(isHit){
                 System.out.println(arrHiddenWord);
+            }else{
+                ++numOfErrors;
+            }
+            paintMan(numOfErrors);
+            System.out.println("Число ошибок:" + numOfErrors);
+
+            if(numOfSuccess == arrWord.length) {
+                System.out.println("Поздравляю, вы выиграли!");
+                break;
             }
 
+            if (numOfErrors==7){
+                System.out.println("Превышено число ошибок. Вы проиграли.");
+                break;
+            }
 
         }
-
-
 
     }
     private static String getWord(){
 
-        String[] WordArr = new String[]{"Слово"};
+        String[] WordArr = new String[]{"слово"};
         return WordArr[0];
 
     }
@@ -66,6 +80,18 @@ public class Main {
     private static String readConsole() throws IOException { //вычитываем что написали
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         return reader.readLine();
+
+    }
+
+    private static void paintMan(int numErrors){
+        System.out.println("╔ ═ ═ ═ ═ ═ ═ ═");
+        System.out.println("║" + (numErrors > 0 ? "           │" : ""));
+        System.out.println("║" + (numErrors > 1 ? "           ◯" : ""));
+        System.out.println("║" + (numErrors > 2 ? "          /" : "") + (numErrors > 3 ? "│" : "") + (numErrors > 4 ? "\\" : ""));
+        System.out.println("║" + (numErrors > 5 ? "          /" : "") + (numErrors > 6 ? " \\" : ""));
+        System.out.println("║");
+        System.out.println("║");
+        System.out.println("╚═ ═ ═ ═ ═ ═ ═ ═ ");
 
     }
 }
