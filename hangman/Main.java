cc099ab1d0487e4c43aaadca18897c77db9dfb38
Mikhail Pashkov <mikhail.pashkov@raiffeisen.ru>
2024-06-10 17:47:17 +0300

    Last steps

--- hangman/Main.java
+++ hangman/Main.java
@@ -7,18 +7,24 @@ import static hangman.WordList.getWord;
 public class Main {
     public static void main(String[] args) throws IOException{
 
-        System.out.println("Это игра Виселица. Чтобы начать игру, введите Start");
+        System.out.println("=======================================================================");
+        System.out.println("Это игра Виселица. Команды:");
+        System.out.println("1 - Начать игру");
+        System.out.println("2 - Выйти");
+        System.out.println("=======================================================================");
 
 
         String inpVal = new String(Utils.readConsole());
 
-        if (inpVal.contains("Start")){
-            System.out.println("Игра началась");
-            System.out.println("Я загадал слово. У тебя есть несколько попыток чтобы его отгадать");
-            iterateOverWord(getWord());
+        if (inpVal.contains("1")){
+            for(;;) {
+                System.out.println("Игра началась");
+                System.out.println("Я загадал слово. У тебя есть несколько попыток чтобы его отгадать");
+                iterateOverWord(getWord());
+            }
 
         }else {
-            System.out.println("Неверная команда, выполнение прекращено");
+            System.out.println("Выбран выход из игры");
             System.exit(0);
         }
     }
