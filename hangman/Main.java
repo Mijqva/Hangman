237a979991fadba44fecd99355f070835a583cf0
Mikhail Pashkov <mikhail.pashkov@raiffeisen.ru>
2024-06-10 18:03:56 +0300

    Last steps

--- hangman/Main.java
+++ hangman/Main.java
@@ -1,12 +1,12 @@
 package hangman;
 import java.io.IOException;
 import static hangman.Game.iterateOverWord;
+import static hangman.Utils.readConsole;
 import static hangman.WordList.getWord;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-
+    public static void main(String[] args) throws IOException {
         System.out.println("=======================================================================");
         System.out.println("Это игра Виселица. Команды:");
         System.out.println("1 - Начать игру");
@@ -14,17 +14,26 @@ public class Main {
         System.out.println("=======================================================================");
 
 
-        String inpVal = new String(Utils.readConsole());
+        String inpVal = new String(readConsole());
 
-        if (inpVal.contains("1")){
-            for(;;) {
+        if (inpVal.equals("1")) {
+            for (;;) {
                 System.out.println("Игра началась");
                 System.out.println("Я загадал слово. У тебя есть несколько попыток чтобы его отгадать");
                 iterateOverWord(getWord());
-            }
+                System.out.println("=======================================================================");
+                System.out.println("Начать новую игру? Команды:");
+                System.out.println("1 - Начать игру");
+                System.out.println("2 - Выйти");
+                System.out.println("=======================================================================");
 
+                inpVal = readConsole();
+                if (inpVal.equals("2")){
+                    System.exit(0);
+                }
+
+            }
         }else {
-            System.out.println("Выбран выход из игры");
             System.exit(0);
         }
     }
